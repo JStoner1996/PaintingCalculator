9b1940852acbdfed0e18eefb40acd4362577ce2b
Jake Stoner <jakestoner123@live.com>
2022-05-16 15:34:43 +0100

    reformatted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,17 @@
 import java.util.Scanner;
 
 public class Main {
+    // Declare variables needed
+    static int wallArea = 0;
+    static int doorsArea = 0;
+     static int socketsArea = 0;
+    static int windowsArea = 0;
+    static int radiatorArea = 0;
+    static int othersArea = 0;
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        // Declare variables needed
-        int wallArea = 0;
-        int doorsArea = 0;
-        int socketsArea = 0;
-        int windowsArea = 0;
-        int radiatorArea = 0;
-        int othersArea = 0;
 
         //Introduction
         System.out.println("\nHello welcome to Paint Calculator 2022.\nIf you do not know the sizes" +
@@ -56,7 +56,7 @@ public class Main {
         double paintSize = myScanner.nextDouble();
         // Find amount of buckets needed
         double exactBucketsNeeded = Math.round((paintAmount / paintSize)*100.0)/100.0;
-        System.out.println("This means " + exactBucketsNeeded + " buckets of paint is needed to cover the walls. Or " +
+        System.out.println("This means " + exactBucketsNeeded + " buckets of paint is needed to cover the walls, or " +
                 ((int)(exactBucketsNeeded * 100)) + "% of a bucket." );
 
         // Rounds buckets needed to two decimal places
@@ -64,16 +64,16 @@ public class Main {
         System.out.println("Rounded up that would be " + bucketsNeeded + " buckets.");
 
         // Find total price of paint
-        System.out.println("Whats the price of the paint you will use in GBP?");
+        System.out.println("\nWhats the price of the paint you will use in GBP?");
         int paintCost = myScanner.nextInt();
         double totalPaintCost = paintCost * bucketsNeeded;
         System.out.println("That would be " + totalPaintCost + " GBP in paint costs.");
+
     }
 
     public static int calculateDoors(int wallNumber) {
         Scanner doorScanner = new Scanner(System.in);
 
-        int doorsArea = 0;
         int doorSize = 15096;
 
         System.out.println("How many doors on wall " +  (wallNumber + 1) + "?");
@@ -105,7 +105,6 @@ public class Main {
     public static int calculateSockets(int wallNumber){
         Scanner socketsScanner = new Scanner(System.in);
 
-        int socketsArea = 0;
         int socketSize = 126;
 
         System.out.println("How many sockets on wall " + (wallNumber + 1) + "?");
@@ -134,7 +133,6 @@ public class Main {
     public static int calculateWindows(int wallNumber){
         Scanner windowScanner = new Scanner(System.in);
 
-        int windowsArea = 0;
         int windowSize = 5575;
 
         System.out.println("How many windows on wall " + (wallNumber + 1) + "?");
@@ -164,7 +162,6 @@ public class Main {
     public static int calculateRadiators(int wallNumber) {
         Scanner radiatorScanner = new Scanner(System.in);
 
-        int radiatorArea = 0;
         int radiatorSize = 4800;
 
         System.out.println("How many radiators on wall " + (wallNumber + 1) + "?");
@@ -194,7 +191,6 @@ public class Main {
     public static int calculateOthers(int wallNumber){
         Scanner othersScanner = new Scanner(System.in);
 
-        int othersArea = 0;
         int othersSize = 0;
 
         System.out.println("Do you have any other objects blocking this wall?");
